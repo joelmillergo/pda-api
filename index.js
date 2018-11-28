6aaed4c33736c700e29289ce7875523065b8d900
杨林海 <18562816056>
2018-11-28 10:48:09 +0800

    test

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const Vision = require('vision');
 const HapiSwagger = require('hapi-swagger');
 const Pack = require('./package');
 
-mongoose.connect('mongodb://test:test123@ds159263.mlab.com:59263/db196');
+mongoose.connect('mongodb://test:test123@ds159263.mlab.com:59263/db196',{ useNewUrlParser: true });
 
 mongoose.connection.once('open',() =>{
-    console.log('connected to database');
+    // console.log('connected to database');
 });
 
 const server = hapi.server({
@@ -37,7 +37,18 @@ const init = async () => {
     
     server.route(routes);
     await server.start();
-    console.log('Server running at: ' + server.info.uri);
+    // console.log('Server running at: ' + server.info.uri);
 };
 
 init();
+
+
+
+
+var Web3 = require('web3');
+var web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider("http://127.0.0.1:8545"));
+ 
+var version = web3.version.node;
+console.log(version);
+ 
